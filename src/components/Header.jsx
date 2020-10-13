450d331c6ee6ead0ac014357a153c8368eb54f3b
Michael-Plat <67632735+Michael-Plat@users.noreply.github.com>
2020-10-13 18:09:47 +0300

    addebt file  ../.DS_Store         ../.gitignore         ../README.md         ../package-lock.json         ../package.json         ../public/         ./

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='Header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/f/f3/RKN_site_logo.png' />
+      </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='Content'>
+        <div>
+        <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'/>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New posts
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default Profile;
