dfe0b60d685682d42a2ca45564cd20dd7502ffba
Michael-Plat <67632735+Michael-Plat@users.noreply.github.com>
2020-10-21 15:18:49 +0300

    № 4 added files: Dialogs.jsx Dialogs.module.css  modified:  src/App.css, src/App.js

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from './MyPosts/MyPosts.jsx';
 import s from './Profile.module.css';
 const Profile = () => {
   return (
-    <div className={s.Content}>
+    <div>
       <div>
         <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg' />
       </div>
