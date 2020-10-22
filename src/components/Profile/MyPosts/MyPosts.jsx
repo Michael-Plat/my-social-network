8f5ac879e1ea84b0a67f140eb082fd34ebb29cfd
Michael-Plat <67632735+Michael-Plat@users.noreply.github.com>
2020-10-22 16:17:17 +0300

    № 9 added files: ProfileInfo.jsx, ProfileInfo.module.css., modified: Profile.jsx, MyPosts.jsx, Dialogs.jsx.

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,12 +3,16 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post.jsx';
 const MyPosts = () => {
   return (
-    <div>
-      My posts
+    <div className={s.postBlock}>
+      <h3>My posts</h3>
       <div>
-        <textarea></textarea>
-        <button>Add post</button>
-          </div>
+        <div>
+          <textarea></textarea>
+        </div>
+        <div>
+          <button>Add post</button>
+        </div>
+      </div>
       <div className={s.posts}>
         <Post message="Hi, how are you?" numberLike='15' />
         <Post message="It' s my fist post" numberLike='20' />
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import MyPosts from './MyPosts/MyPosts.jsx';
 import s from './Profile.module.css';
+import ProfileInfo from './ProfileInfo/ProfileInfo.jsx';
 const Profile = () => {
   return (
     <div>
-      <div>
-        <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg' />
-      </div>
-      <div>
-        ava + description
-        </div>
-      <div>
-        <MyPosts />
-      </div>
+      <ProfileInfo />
+      <MyPosts />
     </div>
   );
 }
